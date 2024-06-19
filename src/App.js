7744025997a5cfc360f318380f35b569df560b9a
JosephTwisted <josephistwisted@gmail.com>
2024-06-20 01:13:22 +0200

    JobVacancyTest3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import JobList from './components/JobList';
 import JobDetail from './components/JobDetail';
 import Login from './components/Login';
-import Signup from './components/Signup';
 import PostJob from './components/PostJob';
 
 function App() {
